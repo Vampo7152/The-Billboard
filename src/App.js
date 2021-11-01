1a8ff58f3bc19721c9e8beee7b0afd367bc9f150
Vampo7152 <umangboss786@gmail.com>
2021-11-01 20:28:37 +0530

    WalletConnect

--- src/App.js
+++ src/App.js
@@ -3,6 +3,48 @@ import twitterLogo from "./assets/twitter-logo.svg";
 import React, { useEffect, useState, useCallback } from "react";
 import { ethers } from "ethers";
 import theBillboard from "./utils/TheBillboard.json";
+import WalletConnect from "@walletconnect/client";
+import QRCodeModal from "@walletconnect/qrcode-modal";
+
+// Create a connector
+const connector = new WalletConnect({
+  bridge: "https://bridge.walletconnect.org", // Required
+  qrcodeModal: QRCodeModal,
+});
+
+// Check if connection is already established
+if (!connector.connected) {
+  // create new session
+  connector.createSession();
+}
+
+// Subscribe to connection events
+connector.on("connect", (error, payload) => {
+  if (error) {
+    throw error;
+  }
+
+  // Get provided accounts and chainId
+});
+
+connector.on("session_update", (error, payload) => {
+  if (error) {
+    throw error;
+  }
+
+  // Get updated accounts and chainId
+  const { accounts, chainId } = payload.params[0];
+});
+
+connector.on("disconnect", (error, payload) => {
+  if (error) {
+    throw error;
+  }
+
+  // Delete connector
+});
+
+
 
 const TWITTER_HANDLE = "Umang_veerma";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
